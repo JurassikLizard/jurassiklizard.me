21c57b603d52df4854576d9e94ea515265fabef4
JurassikLizard <al9999things@gmail.com>
2024-10-02 20:36:28 -0400

    change button name

--- assets/js/home.js
+++ assets/js/home.js
@@ -2,9 +2,9 @@ document.getElementById("pgpToggle").addEventListener("click", function() {
     var pgpKey = document.getElementById("pgpKey");
     if (pgpKey.classList.contains("hidden")) {
         pgpKey.classList.remove("hidden");
-        this.textContent = "Hide Public PGP Key";
+        this.textContent = "Hide PGP Key";
     } else {
         pgpKey.classList.add("hidden");
-        this.textContent = "Show Public PGP Key";
+        this.textContent = "Show PGP Key";
     }
 });
